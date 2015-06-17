b0f691ebf3f50408eea0387f3b88b3acc457a534
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-06-17 13:00:18 +0200

    typo

--- routes/mongomodule.js
+++ routes/mongomodule.js
@@ -2,7 +2,7 @@
 // Copyright (c) Autodesk, Inc. All rights reserved
 // Written by Xiaodong Liang 2015 - ADN/Developer Technical Services
 //
-// Permission to use, copy, modify, and distribute this software in
+// Permission to use, copy, modify, and distribute this softwin
 // object code form for any purpose and without fee is hereby granted,
 // provided that the above copyright notice appears in all copies and
 // that both that copyright notice and the limited warranty and
@@ -22,13 +22,13 @@ var bson =require ('bson') ;
 var BSON = bson.BSONPure.BSON;
 var Long = bson.BSONPure.Long;
 var multipart = require('multipart');
-var sys = require('sys'); 
+var sys = require('sys');
 
 var currentVoiceBinary;
 
 
 // Retrieve mongodb
-var MongoClient = require('mongodb').MongoClient; 
+var MongoClient = require('mongodb').MongoClient;
 var MongoDbName = "mongodb://localhost:27017/testdb";
 var mongoCollectionName = 'voicedemo';
 
@@ -39,32 +39,32 @@ router_mongo.post('/addItemToMongodb', function (req, res) {
 
 	MongoClient.connect(MongoDbName, function(err, db) {
 	  if(!err) {
-	  
-		console.log(new Date().toISOString() +　"MongoDB are connected");
-		var collection = db.collection(mongoCollectionName); 		
-		 
+
+		console.log(new Date().toISOString() +　"MongoDB connected");
+		var collection = db.collection(mongoCollectionName);
+
 		var user=	req.body.user;
 		var viewdbids=	req.body.viewdbids;
-		var blob=	req.body.blob; 
-		var date = req.body.date; 
-		  
+		var blob=	req.body.blob;
+		var date = req.body.date;
+
 		  collection.insert(
 	  	   [
 			  {	user:user,blob:blob,viewdbids:viewdbids,date:date}
-			], function(err, result) {			 
+			], function(err, result) {
 				console.log(new Date().toISOString() +"Inserted 1 item into the document collection");
 				 res.setHeader('Content-Type', 'application/json');
-				 var str = {data:'success'};				 
+				 var str = {data:'success'};
 				  res.send(str);
-				db.close();			 
-		  }); 
+				db.close();
+		  });
 	  }
 	  else
 	  {
-		console.log(new Date().toISOString() +　"MongoDB are NOT connected");
-	  } 
+		console.log(new Date().toISOString() +　"MongoDB NOT connected");
+	  }
 	 });
-      
+
 }) ;
 
 //add multi new items to mongodb collection
@@ -72,53 +72,53 @@ router_mongo.post('/addItemToMongodb', function (req, res) {
 router_mongo.post('/addItemsToMongodb', function (req, res) {
 
 	MongoClient.connect(MongoDbName, function(err, db) {
-	  if(!err) {	   
-		console.log(new Date().toISOString() +　"MongoDB are connected");
-		var collection = db.collection(mongoCollectionName);  
-		 
+	  if(!err) {
+		console.log(new Date().toISOString() +　"MongoDB connected");
+		var collection = db.collection(mongoCollectionName);
+
 		console.log(new Date().toISOString() +　"add items from Mongodb");
-		   
-		  collection.insert(req.body.data, function(err, result) {			 				
-				console.log(new Date().toISOString() +"Inserted multi items into the document collection");				 				
+
+		  collection.insert(req.body.data, function(err, result) {
+				console.log(new Date().toISOString() +"Inserted multi items into the document collection");
 				res.setHeader('Content-Type', 'application/json');
-				var str = {data:'success'};				 
+				var str = {data:'success'};
 				res.send(str);
-				db.close(); 
-		  });  
+				db.close();
+		  });
 	  }
 	  else
 	  {
-	   console.log(new Date().toISOString() +　"MongoDB are NOT connected");
-	  } 
-	 }); 
-   
+	   console.log(new Date().toISOString() +　"MongoDB NOT connected");
+	  }
+	 });
+
 }) ;
 
 //get all items of these mongodb
 router_mongo.get('/getItemsFromMongodb', function (req, res) {
-  
+
   MongoClient.connect(MongoDbName, function(err, db) {
-  
-	  if(!err) {		
-		console.log(new Date().toISOString() +　"MongoDB are connected");
+
+	  if(!err) {
+		console.log(new Date().toISOString() +　"MongoDB connected");
 		var col = db.collection(mongoCollectionName);
 		console.log(new Date().toISOString() +　"get items from Mongodb");
-		
-		col.find({}).toArray(function(err, docs) {   
-			 console.log(new Date().toISOString() +　"Found"　+ docs.length +"records"); 
+
+		col.find({}).toArray(function(err, docs) {
+			 console.log(new Date().toISOString() +　"Found"　+ docs.length +"records");
 			 res.setHeader('Content-Type', 'application/json');
              res.send(docs);
-			 console.log(docs.length);   
+			 console.log(docs.length);
 			 db.close();
-		  }); 
+		  });
 	  }
 	  else
 	  {
-	    console.log(new Date().toISOString() +　"MongoDB are NOT connected");
+	    console.log(new Date().toISOString() +　"MongoDB NOT connected");
 	  }
 	});
-}) ; 
- 
+}) ;
+
 
 
 module.exports =router_mongo ;
