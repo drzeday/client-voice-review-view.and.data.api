a63c2e55ded8c668c43c889416a0216a7e55a17c
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-06-17 10:29:27 +0200

    minimal fixes to get it to run for me

--- credentials_.js
+++ credentials_.js
@@ -20,11 +20,11 @@ var credentials ={
 	credentials: {
 		// Replace placeholder below by the Consumer Key and Consumer Secret you got from
 		// http://developer.autodesk.com/ for the production server
-		client_id: process.env.CONSUMERKEY || '<replace with your consumer key>';
-		client_secret: process.env.CONSUMERSECRET || '<replace with your consumer secret>';
+		client_id: process.env.CONSUMERKEY || '<replace with your consumer key>',
+		client_secret: process.env.CONSUMERSECRET || '<replace with your consumer secret>',
 		grant_type: 'client_credentials'
 	},
-	
+
 	// If you which to use the Autodesk View & Data API on the staging server, change this url
 	BaseUrl: 'https://developer.api.autodesk.com',
 	Version: 'v1'
